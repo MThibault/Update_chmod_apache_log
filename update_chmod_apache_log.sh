6267845b2102ccf3e4064435fed381b4770a369b
MThibault <tmillant@gmail.com>
2016-03-23 12:33:26 +0100

    Creation of a fonction to browse directory

--- update_chmod_apache_log.sh
+++ update_chmod_apache_log.sh
@@ -1,43 +1,48 @@
 #!/usr/bin/env bash
-# Author : Thibault MILLANT
-# Script to update the chmod on the log directory
-# Can be used with crontab to update automatically
+## Author : Thibault MILLANT
+## Script to update the chmod on the log directory
+## Can be used with crontab to update automatically
 
-#chmod o+rx -R /var/log/*
+## Fonction
+# We list directories and files in the current directory
+Browse_directory () {
+	cd $1
+	
+	for file in `ls -l`; do
+		# Get each line
+		echo "File found : $file"
+		# Get the first character to identify the type of file
+		first_letter=${file:0:1}
+		echo "First letter : $first_letter"
+		# Get the last word in the string (File's name)
+		last_word=${file##* }
+		echo "Last word : $last_word"
+
+		case $first_letter in
+			"-" | "l")
+				echo "This is a file or a link."
+				#chmod o+r $last_word
+				;;
+			"d")
+				echo "This is a directory."
+				#chmod o+rx $last_word
+				;;
+			*)
+				echo "Other."
+				;;
+		esac
+	done
+}
 
 # We move to the log directory
-cd /var/log
+directory="/var/log"
 
 # We change the Internal Field Separator (IFS)
 # Space by default
 old_IFS=$IFS
 IFS=$'\n'
 
-# We list directories and files in the current directory
-for file in `ls -l`; do
-	# Get each line
-	echo "File found : $file"
-	# Get the first character to identify the type of file
-	first_letter=${file:0:1}
-	echo "First letter : $first_letter"
-	# Get the last word in the sting (File's name)
-	last_word=${file##* }
-	echo "Last word : $last_word"
-
-	case $first_letter in
-		"-" | "l")
-			echo "This is a file or a link."
-			
-			;;
-		"d")
-			echo "This is a directory."
-			#chmod o+rx $file
-			;;
-		*)
-			echo "Other."
-			;;
-	esac
-done
+Browse_directory $directory
 
 # Set up the default IFS
 ISF=$old_IFS
